e1e9b9555154da60dee4bef8545e6c1eb53be85b
Abdur Rahman <123134811+AbdurRahman22224@users.noreply.github.com>
2024-06-21 19:43:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,7 @@ def predict_and_plot_video(video_path:str, output_path:str)-> str:
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = int(cap.get(cv2.CAP_PROP_FPS))
         fourcc = cv2.VideoWriter_fourcc(*'H264')
+        os.makedirs(os.path.dirname(output_path), exist_ok= True)
         out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
         while cap.isOpened():
             ret, frame = cap.read()
